9c7379e9aa154f7ebf561e96e00283fbd455ed44
msquirrel <manuele.longhi@gmail.com>
2019-04-15 12:24:24 +0200

    Slight improvement to populate
    Populate gives better output and and help information. Defaults for the 
    file location and sprites folder has been changed to the base directory.
    Changed .gitignore in order to avoid pushing those files

--- javagochi/management/commands/populate.py
+++ javagochi/management/commands/populate.py
@@ -3,9 +3,13 @@ from django.core.files import File
 from javagochi.models import JavagochiBase
 import pandas as pd
 import os
+from javagochi_server.settings import BASE_DIR
 
-file = ("C:\\Users\\Manu\\Downloads\\Javagochi classes.xlsx")
-sprites_dir = ("C:\\Users\\Manu\\Downloads\\Sprites")
+FILE_NAME = "Javagochi classes.xlsx"
+SPRITES_NAME_DIR = "sprites"
+
+file = os.path.join(BASE_DIR, FILE_NAME)
+sprites_dir = os.path.join(BASE_DIR, SPRITES_NAME_DIR)
 
 # At index 0 there is a table that indicates the stats of the Javagochis in this order:
 # -1: Name
@@ -20,27 +24,11 @@ sprites_dir = ("C:\\Users\\Manu\\Downloads\\Sprites")
 # -10: Min. user level
 
 class Command(BaseCommand):
-    help = 'Populates the database'
+    help = 'Populates the database. The excel file and the sprites folder need to be located at the root directory. If not you need to indicate the location with -f and -s respectively'
 
     def add_arguments(self, parser):
         parser.add_argument('-f', '--file', type=str, help='Define the file location', )
         parser.add_argument('-s', '--sprites', type=str, help='Define the sprites location', )
-
-    # def _excel_to_dict(self, file, index):
-    #     dict = {
-    #         "name" = df['Name'][index],
-    #         "health" = df['Health'][index],
-    #         "hunger" = df['Hunger'][index],
-    #         "cold" = df['Cold'][index],
-    #         "hot" = df['Hot'][index],
-    #         "age" = df['Age'][index],
-    #         "evolves_at" = df['Evolves at'][index],
-    #         "evolves_into" = df['Evolves Into'][index],
-    #         "coins" = df['Coins needed'][index],
-    #         "usr_lvl" = df['Min level'][index]
-    #     }
-    #
-    #     return dict
 
     def retrieve_race_line(self, file, race):
         for i in file.index:
@@ -49,7 +37,7 @@ class Command(BaseCommand):
 
     def create_javagochi(self, df, index, sprites):
         name = df['Name'][index]
-        print("Adding {}".format(name))
+        self.stdout.write("Adding {} to database...".format(name))
         health = df['Health'][index]
         hunger = df['Hunger'][index]
         cold = df['Cold'][index]
@@ -62,7 +50,7 @@ class Command(BaseCommand):
         exp = df['Exp'][index]
 
         if(JavagochiBase.objects.filter(race=name).first()):
-            print("Already in database")
+            self.stdout.write("{} was already in database".format(name))
             return
 
         jc = JavagochiBase(race=name)
@@ -81,12 +69,14 @@ class Command(BaseCommand):
         if(df['Evolves into'][index] != '-' and df['Evolves into'][index] != ''):
             evolution = JavagochiBase.objects.filter(race=evolves_into).first()
             if(JavagochiBase.objects.filter(race=evolves_into).count() == 0):
+                self.stdout.write("{} evolves into not-yet-existing Javagochi({}). Adding evolution first...".format(name, evolves_into))
                 index = self.retrieve_race_line(df, evolves_into)
                 self.create_javagochi(df, index, sprites)
                 evolution = JavagochiBase.objects.filter(race=evolves_into).first()
             jc.evolves_at = evolves_at
             jc.evolves_into = evolution
         jc.save()
+        self.stdout.write("{} has been added to the database".format(name))
 
     def handle(self, *args, **options):
         if(options['file']):
