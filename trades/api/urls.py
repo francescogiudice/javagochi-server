4bbbf9f63eb7c0ea50e7dd732cb7f2e0b897624b
msquirrel <manuele.longhi@gmail.com>
2019-04-16 13:39:38 +0200

    Exposed create trade API

--- trades/api/urls.py
+++ trades/api/urls.py
@@ -2,10 +2,12 @@ from django.urls import path
 
 from .views import (
     TradeOffersListView,
-    ParticularTradeOfferView
+    ParticularTradeOfferView,
+    StartTradeView
 )
 
 urlpatterns = [
     path('all/', TradeOffersListView.as_view()),
+    path('add/', StartTradeView.as_view()),
     path('<id>/', ParticularTradeOfferView.as_view())
 ]
--- trades/api/views.py
+++ trades/api/views.py
@@ -1,7 +1,10 @@
-from rest_framework.generics import ListAPIView, RetrieveAPIView
+from rest_framework.response import Response
+from rest_framework import status
+from rest_framework.generics import ListAPIView, RetrieveAPIViewm, CreateAPIView
 
 from trades.models import TradeOffer
 from .serializers import TradeOfferSerializer
+from javagochi.models import Javagochi, JavagochiBase
 
 class TradeOffersListView(ListAPIView):
     queryset = TradeOffer.objects.all()
@@ -16,3 +19,19 @@ class ParticularTradeOfferView(RetrieveAPIView):
         url_id = self.kwargs['id']
         trade = queryset.get(id=url_id)
         return trade
+
+class StartTradeView(CreateAPIView):
+    queryset = TradeOffer.objects.all()
+    serializer_class = TradeOfferSerializer
+
+    def create(self, validated_data):
+        print(self.request.data)
+        offered_id = self.request.data['offered_id']
+        interested_into_race = self.request.data['interested_into']
+
+        offered = Javagochi.objects.filter(id=offered_id)
+        interested_into = JavagochiBase.objects.filter(race=interested_into_race)
+
+        trade = Trade(offered=offered, interested_into=interested_into)
+        trade.save()
+        return Respone("Trade started", status=status.HTTP_201_CREATED)
