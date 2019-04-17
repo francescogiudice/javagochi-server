daccc4638ccb4458ad926ad951a86b58006778d2
msquirrel <manuele.longhi@gmail.com>
2019-04-18 01:00:18 +0200

    Completed trade functionality.
    Exposed the conclude trade API. Implemented the functionality to 
    conclude the trade in the fronted

--- trades/api/urls.py
+++ trades/api/urls.py
@@ -4,12 +4,14 @@ from .views import (
     TradeOffersListView,
     ParticularTradeOfferView,
     StartTradeView,
-    CloseTradeView
+    DeleteTradeView,
+    ConcludeTradeView
 )
 
 urlpatterns = [
     path('all/', TradeOffersListView.as_view()),
     path('add/', StartTradeView.as_view()),
     path('<id>/', ParticularTradeOfferView.as_view()),
-    path('<id>/close', CloseTradeView.as_view())
+    path('<id>/close', DeleteTradeView.as_view()),
+    path('<id>/conclude/', ConcludeTradeView.as_view())
 ]
--- trades/api/views.py
+++ trades/api/views.py
@@ -1,7 +1,7 @@
 from datetime import datetime
 from rest_framework.response import Response
 from rest_framework import status
-from rest_framework.generics import ListAPIView, RetrieveAPIView, CreateAPIView, DestroyAPIView
+from rest_framework.generics import ListAPIView, RetrieveAPIView, CreateAPIView, DestroyAPIView, UpdateAPIView
 
 from trades.models import TradeOffer
 from .serializers import TradeOfferSerializer
@@ -38,7 +38,7 @@ class StartTradeView(CreateAPIView):
         trade.save()
         return Response("Your Javagochi is now being traded", status=status.HTTP_201_CREATED)
 
-class CloseTradeView(DestroyAPIView):
+class DeleteTradeView(DestroyAPIView):
     queryset = TradeOffer.objects.all()
     serializer_class = TradeOfferSerializer
 
@@ -51,3 +51,32 @@ class CloseTradeView(DestroyAPIView):
         trade = TradeOffer.objects.filter(id=self.kwargs['id']).first()
         self.perform_destroy(trade)
         return Response("Eliminated trade", status=status.HTTP_200_OK)
+
+class ConcludeTradeView(UpdateAPIView):
+    queryset = TradeOffer.objects.all()
+    serializer_class = TradeOffer
+
+    def update(self, request, *args, **kwargs):
+        print(request.data)
+        trade_id = kwargs['id']
+        id_trader = request.data['id_trader']
+
+        trade = TradeOffer.objects.filter(id=trade_id).first()
+        jc1 = trade.offering
+        jc2 = Javagochi.objects.filter(id=id_trader).first()
+
+        print(jc1.nickname + "->" + jc1.owner.username)
+        print(jc2.nickname + "->" + jc2.owner.username)
+
+        usr_tmp = jc2.owner
+        jc2.owner = jc1.owner
+        jc1.owner = usr_tmp
+
+        print(jc1.nickname + "->" + jc1.owner.username)
+        print(jc2.nickname + "->" + jc2.owner.username)
+
+        jc1.save()
+        jc2.save()
+        trade.delete()
+
+        return Response("Correctly traded the Javagochis", status=status.HTTP_200_OK)
