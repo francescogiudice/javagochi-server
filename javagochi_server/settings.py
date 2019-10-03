d4e23dea6519f0bcae93a88fe762a9b2f664699a
francescogiudice <aaff00@libero.it>
2019-10-03 14:00:26 +0200

    changed BASE_DIRECTORY to /var/www/

--- javagochi_server/settings.py
+++ javagochi_server/settings.py
@@ -14,7 +14,7 @@ from celery.schedules import crontab
 import os
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
-BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+BASE_DIR = "/var/www/"
 
 
 # Quick-start development settings - unsuitable for production
@@ -121,7 +121,7 @@ WSGI_APPLICATION = 'javagochi_server.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join('/var/www/', 'db.sqlite3'),
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
 
