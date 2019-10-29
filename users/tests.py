d9e5a7036d61b0e5c8fe1e5dffbb52699b2a4f7d
francescogiudice <aaff00@libero.it>
2019-10-29 16:57:50 +0100

    created basic tests to start testing

--- users/tests.py
+++ users/tests.py
@@ -1,3 +1,18 @@
 from django.test import TestCase
+from users.models import CustomUser
+from users.scripts import increase_user_level
+from django.test import Client
 
-# Create your tests here.
+class TestScripts(TestCase):
+
+    def test_users_script(self):
+        test_exp = 50
+        user = CustomUser.objects.filter(username='fra').first()
+        old_exp = user.exp
+        increase_user_level(user, test_exp)
+        self.assertEqual(user.exp, old_exp + test_exp)
+
+    def test_user_login(self):
+        c = Client()
+        response = c.post('/rest-auth/login/', {'username': 'fra', 'password': 'fra'})
+        self.assertEqual(response.status_code, 200)
